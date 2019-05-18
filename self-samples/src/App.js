dada0d1251a3acbecdca300573e8584722291724
amandeep.bajwa <amandeep.bajwa@care.com>
2019-05-18 15:13:28 +0530

    Added Sample component

---
+++ self-samples/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import Sample from './components/Sample';
+
+function App() {
+  return (
+  <div className="App">
+    <Sample />
+  </div>
+  );
+}
+
+export default App;
---
+++ self-samples/src/components/Sample.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Sample() {
+  return (
+    <h1>
+      Hi. I am a sample ReactComponent.
+    </h1>
+  )
+}
+
+export default Sample

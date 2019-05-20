09e613ae53ab0209f13074b2fa8e59d59fc5abb8
Sumit Kumar Upadhyay <sumit.kumar.upadhyay@gmail.com>
2019-05-20 10:09:08 +0530

    Create service.js

---
+++ self-samples/src/service.js
@@ -0,0 +1,9 @@
+export default function userDetails() {
+    // make API Call
+    return {
+      // sample, hard-cooded API response
+      name: "Amandeep Singh",
+      twitterHandle: "learn_n_share"
+    };
+  }
+  
